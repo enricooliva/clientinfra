4fce72e7179ca3a053b5a9bb923cea5c4164a208
Enrico Oliva <enrico.oliva@uniurb.it>
2018-06-12 14:32:51 +0200

    installato npm bootrap italia submission service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { NgForm }   from '@angular/forms';
+import { SubmissionService } from './submission.service';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Unipeo client';
+  errorMessage = '';
+
+  submissions$: Observable<any>;
+  constructor(private submissionService: SubmissionService) {}
+
+  ngOnInit(){
+    //modalità asincrona 
+    //this.submissions$ = this.submissionService.getSumbissions();
+    this.submissionService.getSumbissions()
+    .subscribe((data)=> {
+      this.submissions$ = data;
+    },
+    (err: HttpErrorResponse) => {
+      if (err.error instanceof Error) {
+        // client-side error
+        this.errorMessage = `An error occured: ${err.error.message}`;
+      } else {
+        this.errorMessage = `Backend returned code ${err.status}, body was: ${err.message}`;
+      }
+    });
+  }
+
+  onSubmit(form: NgForm){
+    console.log('ci sono');
+    console.log(form.value);
+  }
 }
