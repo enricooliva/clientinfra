b83dfb18b2b02a6ddc6ec6e4cde5309703db3b17
Enrico Oliva <enrico.oliva@uniurb.it>
2019-07-30 11:47:16 +0200

    feat: cambio config

--- src/environments/environment.preprod.ts
+++ src/environments/environment.preprod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   API_URL: 'https://unidem-preprod.uniurb.it/unidem/uniconv/uniconv/public/',
-  baseHref: '/unidem/uniconv/uniconvclient/',
+  baseHref: '/',
   whitelistedDomains: ['unidem-preprod.uniurb.it'],
   blacklistedRoutes: ['https://unidem-preprod.uniurb.it/unidem/uniconv/uniconv/public/auth/'],
 };
